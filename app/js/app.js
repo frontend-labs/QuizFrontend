1a567e931a0155e1f74e4822c59f1a174d7e6e56
Luis Felipe <lfna23@gmail.com>
2014-12-30 16:17:21 -0500

    feat(Questions): Se agregó la fabrica que retorna la lista de preguntas

--- app/js/app.js
+++ app/js/app.js
@@ -18,3 +18,304 @@ app.config(function($routeProvider) {
 		redirectTo: '/'
 	});
 });
+
+
+
+
+
+//Fabrica que retorna la lista de preguntas
+app.factory('Questions', function(){
+	return [
+		{
+			id: 1,
+			premise: '¿Cómo se produce la inyección de dependencias en AngularJS?',
+			keys: [
+				{
+					id: 1,
+					text: 'AngularJS posee un ente que trabaja como inyector',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'La inyección de dependencias no existe en angularJS',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'Cualquier componente de AngularJS puede ser requerido por otro componente como si fuera un parámetro',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'Las alternativas 1 y 3 son correctas',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 4
+		},
+		{
+			id: 2,
+			premise: '¿Cuál es la definición correcta de un closure?',
+			keys: [
+				{
+					id: 1,
+					text: 'Los closures son funciones que manejan variables independientes. En otras palabras, la función definida en el closure "recuerda" el entorno en el que se ha creado.',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'Son funciones que retornan algo necesariamente',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'Son funciones comunes',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'funciones especiales dentro de otras funciones',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 1
+		},
+		{
+			id: 3,
+			premise: 'Node.JS es:',
+			keys: [
+				{
+					id: 1,
+					text: 'Un servidor de eventos',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'Un servidor de prototipos',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'Una libreria de frontend',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'Un servidor que solo permite ejecutar proyectos con stack Javascript',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 1
+		},
+		{
+			id: 4,
+			premise: '¿Cuál de estos es el contexto de Node?',
+			keys: [
+				{
+					id: 1,
+					text: 'window',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'global',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'document',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'local',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 2
+		},
+		{
+			id: 5,
+			premise: 'Una de estas funciones está mal escrita:',
+			keys: [
+				{
+					id: 1,
+					text: 'function hola(){}',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'var hola = function hola(){}',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'var hola = function(){}',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'function hola(){}',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 2
+		},
+		{
+			id: 6,
+			premise: 'new Ninja("Carlos")',
+			keys: [
+				{
+					id: 1,
+					text: 'Es un objeto',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'Inicializa un objeto',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'Inicializa un objeto Ninja con un parámetro',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'Inicializa un objeto con un apuntador y pasa un parámetro',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 4
+		},
+		{
+			id: 7,
+			premise: '¿Javascript es single thread o “hilo único”?',
+			keys: [
+				{
+					id: 1,
+					text: 'Si',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'No',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'sí, porqué solo hay un evento en ejecución',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'no, porque hay muchos eventos ejecutándose',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 3
+		},
+		{
+			id: 8,
+			premise: 'Scope:',
+			keys: [
+				{
+					id: 1,
+					text: 'Es el contexto en el que se inicializa una variable, función u objeto',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'Es donde creo un objeto y se mantiene',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'Es el contexto de un objeto',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'Es la parte donde inicializo las variables',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 1
+		},
+		{
+			id: 9,
+			premise: 'De estas afirmaciones ¿cuál no es correcta?',
+			keys: [
+				{
+					id: 1,
+					text: 'Se puede crear una función de forma literal',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'A una función se le puede pasar argumentos',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'Una función puede retornar argumentos',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'A una función se le deben crear sus propiedades dinámicamente y asignarlas',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 4
+		},
+		{
+			id: 10,
+			premise: 'Prototype:',
+			keys: [
+				{
+					id: 1,
+					text: 'Es la forma de agregar un método a un objeto',
+					active: false
+				},
+				{
+					id: 2,
+					text: 'Es la parte de un objeto que extiende su funcionalidad',
+					active: false
+				},
+				{
+					id: 3,
+					text: 'Es una propiedad de un objeto',
+					active: false
+				},
+				{
+					id: 4,
+					text: 'Es la propiedad de un objeto que extiende su funcionalidad',
+					active: false
+				}
+			],
+			status: true,
+			visible: false,
+			response: 2
+		}
+
+	];
+});
