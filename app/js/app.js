d28643c22c83d6c3dbe50be0342ea273c7abc503
Luis Felipe <lfna23@gmail.com>
2014-12-30 16:07:07 -0500

    feat(route): Agregando la configuración de rutas al modulo

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,20 @@
 var app = angular.module('myApp',['ngRoute']);
+
+//configuración de rutas
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'templates/inicio.html'
+	})
+	.when('/examen', {
+		controller: 'questionController',
+		templateUrl: 'templates/examen.html'
+	})
+	.when('/resultado', {
+		controller: 'resultController',
+		templateUrl: 'templates/resultado.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+});
